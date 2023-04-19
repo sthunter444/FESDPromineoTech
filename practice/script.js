ee67e30eaca0c5e9cf9c63ad2f256754d3dbd717
sthunter444 <sthunter444@aol.com>
2023-04-19 16:03:11 -0700

    initial add of files

---
+++ practice/script.js
@@ -0,0 +1,11 @@
+console.log ("Hello World")
+
+
+
+var ageRequiredToDrive = 16;
+var currentAge = 16;
+var canPersonDrive = currentAge >= ageRequiredToDrive;
+if (canPersonDrive) {
+console.log('This person can drive')
+
+}
